84fcdc59c30601d8a7a67438a1b587624df996c1
everson <freitas.everson@gmail>
2013-04-01 21:56:08 -0300

    Alterado 3

--- Chibata.java
+++ Chibata.java
@@ -1,5 +1,8 @@
 public class Chibata {
 	public static void main(String []args) {
 		System.out.println("hello World!!!");
+		System.out.println("Chibata no balde");
+
+		System.out.println("aaaa");
 	}
 }
